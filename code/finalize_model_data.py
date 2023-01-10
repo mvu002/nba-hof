12d397371dcda2a3f119681deda77449be6c442c
Max Vu <mvu@berkeley.edu>
2023-01-10 14:25:02 -0800

    combined dataframes of median stats for both HOF and non-HOF, added combined dataframe to data folder.

--- code/finalize_model_data.py
+++ code/finalize_model_data.py
@@ -16,4 +16,22 @@ non_hof_players_stats = all_players_stats[all_players_stats['PLAYER_ID'].isin(ho
 non_hof_players_median_stats = non_hof_players_stats.drop(columns = ['SEASON_ID', 'LEAGUE_ID', 'TEAM_ID', 'TEAM_ABBREVIATION',
 																	 'PLAYER_AGE'])
 non_hof_players_median_stats = non_hof_players_median_stats.groupby(by = "PLAYER_ID").median()
-print(non_hof_players_median_stats)
+#print(non_hof_players_median_stats)
+
+# Aggregate median of each HOF players' stats
+hof_players_stats = pd.read_csv("../data/hof_player_stats.csv")
+hof_players_median_stats = hof_players_stats.drop(columns = ['SEASON_ID', 'LEAGUE_ID', 'TEAM_ID', 'TEAM_ABBREVIATION',
+															 'PLAYER_AGE'])
+hof_players_median_stats = hof_players_median_stats.groupby(by = "PLAYER_ID").median()
+#print(hof_players_median_stats)
+
+# Add "isHOF" variable to dataframe of HOF stats, set every value = 1
+hof_players_median_stats['isHOF'] = 1
+
+# Add "isHOF" variable to dataframe of non-HOF stats, set every value = 0 
+non_hof_players_median_stats['isHOF'] = 0 
+
+# Combine HOF and non-HOF stats dataframes together
+combined_data = hof_players_median_stats.append(non_hof_players_median_stats)
+combined_data.to_csv("../data/all_players_median_stats.csv")
+print(combined_data)
