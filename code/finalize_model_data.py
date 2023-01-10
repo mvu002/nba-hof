64e60f96640ed2bc67367163d19f1ab27d290015
Max Vu <mvu@berkeley.edu>
2023-01-10 13:58:53 -0800

    added table of all players' stats to data folder, added python script that finalizes dataframe for models

---
+++ code/finalize_model_data.py
@@ -0,0 +1,5 @@
+#### Create dataframe of stats of HOFs and non-HOFs
+
+# Extract stats of all players from API 
+all_players_stats = pd.read_csv("../data/all_players_stats.csv")
+print(all_players_stats)
