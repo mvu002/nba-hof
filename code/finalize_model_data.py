c20a2621849e10672baa4828861c8375d9b84888
Max Vu <mvu@berkeley.edu>
2023-01-10 14:11:46 -0800

    finalized table of non-HOF players' stats

--- code/finalize_model_data.py
+++ code/finalize_model_data.py
@@ -11,3 +11,9 @@ all_players_stats = pd.read_csv("../data/all_players_stats.csv")
 hof_player_profiles = pd.read_csv("../data/hof_players_profiles.csv")
 non_hof_players_stats = all_players_stats[all_players_stats['PLAYER_ID'].isin(hof_player_profiles["id"]) == False]
 #print(non_hof_players_stats)
+
+# Aggregate median of each non-HOF players' stats
+non_hof_players_median_stats = non_hof_players_stats.drop(columns = ['SEASON_ID', 'LEAGUE_ID', 'TEAM_ID', 'TEAM_ABBREVIATION',
+																	 'PLAYER_AGE'])
+non_hof_players_median_stats = non_hof_players_median_stats.groupby(by = "PLAYER_ID").median()
+print(non_hof_players_median_stats)
